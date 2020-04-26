eb0ef9c1f8376fee47ae79200f2f4757aea0b4a2
lmw <1339187809@qq.com>
2020-04-26 11:22:29 +0800

    测试在idea中使用git

--- src/main/java/java/test/Happy.java
+++ src/main/java/java/test/Happy.java
@@ -1,7 +1,7 @@
 package java.test;
 
 public class Happy {
-	public static void main(String[] args) {
-		System.out.println("Happy!");
-	}
+    public static void main(String[] args) {
+    	System.out.println("Happy!");
+    }
 }
---
+++ src/main/java/java/test/TestIdeaUseGit.java
@@ -0,0 +1,12 @@
+package java.test;
+
+/**
+ * @author lmw
+ * @date 2020/4/26 - 11:15
+ */
+public class TestIdeaUseGit {
+
+    public static void main(String[] args) {
+        System.out.println("测试在idea中使用git");
+    }
+}
